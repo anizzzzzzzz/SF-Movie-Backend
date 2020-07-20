54dfaecf03a5e62d96ac1f84043b027c2c29a88e
aniz <anish.maharjan@infotmt.com>
2020-07-20 14:06:46 +0545

    removed cors

--- src/test/java/com/anizzzz/sfmovies/service/MovieDataServiceTest.java
+++ src/test/java/com/anizzzz/sfmovies/service/MovieDataServiceTest.java
@@ -1,9 +1,7 @@
 package com.anizzzz.sfmovies.service;
 
-import com.anizzzz.sfmovies.model.MovieData;
 import com.anizzzz.sfmovies.repository.MovieDataRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +9,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.mockito.Mockito.when;
 
@@ -26,15 +20,6 @@ public class MovieDataServiceTest {
     @Autowired
     @Qualifier("movieDataService")
     private IMovieDataService movieServiceMock;
-    @MockBean
-    private RestTemplate restTemplate;
-
-    private Set<MovieData> movieDataSet;
-
-    @Before
-    public void setup(){
-        movieDataSet = new HashSet<>();
-    }
 
     @Test
     public void checkIfDataIsInitialized_IfNoDataInDB(){
