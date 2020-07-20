9874be93a6c4e4cc8ec95dc7aab73d29f0a00379
aniz <anish.maharjan@infotmt.com>
2020-07-20 13:58:56 +0545

    added heroku db config and heroku front-end url for cors

--- src/main/java/com/anizzzz/sfmovies/config/WebConfig.java
+++ src/main/java/com/anizzzz/sfmovies/config/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000", "https://sf-movie-frontend.herokuapp.com")
+//                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
             }
         };
